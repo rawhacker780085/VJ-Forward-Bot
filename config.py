65e606221b6f59a2ac2c490004b276e6f270aea2
rawhacker780085 <153677901+rawhacker780085@users.noreply.github.com>
2025-01-14 16:13:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ from os import environ
 class Config:
     API_ID = int(environ.get("API_ID", "28372387")
     API_HASH = environ.get("API_HASH", "11edc85521fb2d8f75e751bb284507e1")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7760674213:AAEtsGvqzQnRXOJuiSoXIA_gyNOMzjCJcfE") 
     BOT_SESSION = environ.get("BOT_SESSION", "@freenotesforwordbot") 
     DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://rahulkumar200001002:j0W8JF5RWqXOguvM@cluster0.jmmzh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "freenotesforwordbot")
